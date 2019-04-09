3215faf20b73cb46bfa2fd8590b43482ff52bda1
hyx0217 <1014959288@qq.com>
2019-04-09 18:00:55 +0800

    8

--- my-mpvue/src/pages/counter/index.vue
+++ my-mpvue/src/pages/counter/index.vue
@@ -1,111 +1,115 @@
 <template>
-  <div id='cart'>
-      <scroll-view scroll-y='true' >
-              <view v-for="(item, index) in cartProduct" :key="index" style="margin-top:30rpx;width:100%">
-                 
-                  <a class='cartCard'> 
-                      <van-checkbox name='selcetCart' :value="item.ischeck" @change="onSelcet(index)"></van-checkbox>
-                      <van-card
-                          :num='item.num'
-                          :thumb-link="'/pages/prodetails/main?id='+item.id"
-                          centered=true
-                          :price="item.price"
-                          desc="描述信息"
-                          :title="item.name"
-                          :thumb="item.img "
-                      >
-                       <view slot="footer">
-                          <van-stepper :value="item.num" @change="onChangeNum($event,index)" />
-                        </view>
-                      </van-card>
-                  </a>
-              </view>
-       </scroll-view>
-       <van-submit-bar
-          :price="total "
-          button-text="提交订单"
-          @submit="onClickButton"
-        >
-          <van-checkbox :value="checked " @change="onSelectAll">全选</van-checkbox>
-        </van-submit-bar>
+  <div id="cart">
+    <scroll-view scroll-y="true">
+      <view v-for="(item, index) in cartProduct" :key="index" style="margin-top:30rpx;width:100%">
+        <a class="cartCard">
+          <van-checkbox name="selcetCart" :value="item.ischeck" @change="onSelcet(index)"></van-checkbox>
+          <van-card
+            :num="item.num"
+            :thumb-link="'/pages/prodetails/main?id='+item.id"
+            centered="true"
+            :price="item.price"
+            desc="描述信息"
+            :title="item.name"
+            :thumb="item.img "
+          >
+            <view slot="footer">
+              <van-stepper :value="item.num" @change="onChangeNum($event,index)"/>
+            </view>
+          </van-card>
+        </a>
+      </view>
+    </scroll-view>
+    <van-submit-bar :price="total " button-text="提交订单" @submit="onClickButton">
+      <van-checkbox :value="checked " @change="onSelectAll">全选</van-checkbox>
+    </van-submit-bar>
   </div>
 </template>
 
 <script>
-var Fly=require("flyio/dist/npm/wx") 
-var fly=new Fly
-export default{
-  data(){
-    return{
-      baseUrl:process.env.API_ROOT,
-      cartProduct:'',
-      checked:false,
-      check:[],
-      total:0,
-    }
+var Fly = require("flyio/dist/npm/wx");
+var fly = new Fly();
+export default {
+  data() {
+    return {
+      baseUrl: process.env.API_ROOT,
+      cartProduct: "",
+      checked: false,
+      check: [],
+      total: 0
+    };
   },
-  methods:{
-    getDetail(){
-      fly.get(`${this.baseUrl}/product?_limit=8&_page=1`).then(res=>{
-        this.cartProduct=res.data;
-      })
+  methods: {
+    getDetail() {
+      fly.get(`${this.baseUrl}/product?_limit=8&_page=1`).then(res => {
+        this.cartProduct = res.data;
+      });
     },
-    onSelectAll(){
-      if(this.checked){
-        this.check=[];//反选设空
+    onSelectAll() {
+      if (this.checked) {
+        this.check = []; //反选设空
         this.cartProduct.forEach(element => {
-          element.ischeck=false;
+          element.ischeck = false;
         });
-        this.checked=false;
-        this.total=0;//价格归零
-      }else{
-        this.check=[];
+        this.checked = false;
+      } else {
+        this.check = [];
         this.cartProduct.forEach(element => {
-          element.ischeck=true;
-          this.total+=element.price*element.num;
-          this.check.push(element.ischeck)//全选 插入
-        })
-        this.checked=true;
+          element.ischeck = true;
+          this.check.push(element.ischeck); //全选 插入
+        });
+        this.checked = true;
       }
     },
-    onSelcet(i){
-      if(this.cartProduct[i].ischeck){
-        this.total-=this.cartProduct[i].price*this.cartProduct[i].num
-        
-        this.check.pop();//取消选择 移除一个
-        this.cartProduct[i].ischeck=false
-        this.checked=false;
-      }else{
-        this.total+=this.cartProduct[i].price*this.cartProduct[i].num;
-        this.check.push(true);//选择 插入一个
-        this.cartProduct[i].ischeck=true
-        if(this.check.length==this.cartProduct.length){//判断两个数组长度是否相等 全选是否勾上
-          this.checked=true;
+    onSelcet(i) {
+      if (this.cartProduct[i].ischeck) {
+        this.check.pop(); //取消选择 移除一个
+        this.cartProduct[i].ischeck = false;
+        this.checked = false;
+      } else {
+        this.check.push(true); //选择 插入一个
+        this.cartProduct[i].ischeck = true;
+        if (this.check.length == this.cartProduct.length) {
+          //判断两个数组长度是否相等 全选是否勾上
+          this.checked = true;
         }
       }
     },
     //改变数量
-    onChangeNum($event,i){
-      this.cartProduct[i].num=$event.mp.detail;
+    onChangeNum($event, i) {
+      this.cartProduct[i].num = $event.mp.detail;
     }
   },
-  mounted(){
+  mounted() {
     this.getDetail();
   },
-}
-
+  watch: {
+    //监听计算总价
+    cartProduct: {
+      handler: function(val, oldval) {
+        this.total=0;
+        for(var value in val){
+          if(val[value].ischeck==true){
+            this.total+=val[value].num*val[value].price
+          }
+        }
+      },
+      deep: true
+    }
+  }
+};
 </script>
 
 <style>
-::-webkit-scrollbar{
-    width: 0;
-    height: 0;
-    color: transparent;
+::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+  color: transparent;
 }
-.van-card{
-    background: #ffffff!important;
+.van-card {
+  background: #ffffff !important;
 }
-page{
+page {
   width: 100%;
   height: 100%;
 }
@@ -113,20 +117,18 @@ page{
   width: 100%;
   height: 100%;
 }
-#cart scroll-view{
+#cart scroll-view {
   width: 94%;
   height: 90%;
-  margin:0 auto;
-
+  margin: 0 auto;
 }
-.cartCard{
+.cartCard {
   display: flex;
   align-items: center;
   width: 100%;
   background: #ffffff;
 }
-.cartCard van-card{
-
-  width: 100%
+.cartCard van-card {
+  width: 100%;
 }
 </style>
--- my-mpvue/src/pages/prodetails/index.vue
+++ my-mpvue/src/pages/prodetails/index.vue
@@ -1,113 +1,103 @@
 <template>
-    <div id='goods'>
-        <img :src='product.img'>
-        <van-cell-group>
-            <van-cell
-              :title="product.name"
-              :value="'￥'+product.price"
-            />
-        </van-cell-group>
-        <van-cell-group>
-          <van-cell
-            title="运费￥0.00-20.00"
-            :value="'剩余:'+product.price"
-          />
-        </van-cell-group>
-        <van-goods-action>
-          <van-goods-action-icon icon="chat-o" text="客服" />
-          <van-goods-action-icon icon="cart-o" text="购物车" info="5" />
-          <van-goods-action-icon icon="shop-o" text="店铺" />
-          <van-goods-action-button text="加入购物车" type="warning"  @click="onShow"/>
-          <van-goods-action-button text="立即购买" />
-        </van-goods-action>
-        <van-popup
-          :show=" show"
-          position="bottom"
-          @close="onClose"
-        >
-        <div class="modal-top">
-          <van-row>
-            <van-col span="4" offset="2">
-              <div style='border: 1px solid #c3c3c3;width:100rpx;height:100rpx;'>
-                  <img :src='product.img' style='width:100%;height:100%'>
-              </div> 
-            </van-col>
-            <van-col span="16" offset="1">
-              <p>{{product.name}}</p>
-              <span class="price">￥{{product.price}}</span>
-            </van-col>
-          </van-row>
-        </div>
-        <div style="margin-top:15rpx;">
-          <van-row style='margin-bottom:15rpx'>
-            <van-col span='15' offset="1" class='modal-left'>
-              <p>购买数量:</p>
-              <p>剩余:{{product.price}}件</p>
-            </van-col>
-            <van-col span='8'>
-              <van-stepper :value="1" @change="onChange" />
-            </van-col>
-          </van-row>
-          <van-goods-action-button text="确定"/>
-        </div>
-        </van-popup>
-    </div>
-     
+  <div id="goods">
+    <img :src="product.img">
+    <van-cell-group>
+      <van-cell :title="product.name" :value="'￥'+product.price"/>
+    </van-cell-group>
+    <van-cell-group>
+      <van-cell title="运费￥0.00-20.00" :value="'剩余:'+product.price"/>
+    </van-cell-group>
+    <van-goods-action>
+      <van-goods-action-icon icon="chat-o" text="客服"/>
+      <van-goods-action-icon icon="cart-o" text="购物车" info="5"/>
+      <van-goods-action-icon icon="shop-o" text="店铺"/>
+      <van-goods-action-button text="加入购物车" type="warning" @click="onShow"/>
+      <van-goods-action-button text="立即购买"/>
+    </van-goods-action>
+    <van-popup :show=" show" position="bottom" @close="onClose">
+      <div class="modal-top">
+        <van-row>
+          <van-col span="4" offset="2">
+            <div style="border: 1px solid #c3c3c3;width:100rpx;height:100rpx;">
+              <img :src="product.img" style="width:100%;height:100%">
+            </div>
+          </van-col>
+          <van-col span="16" offset="1">
+            <p>{{product.name}}</p>
+            <span class="price">￥{{product.price}}</span>
+          </van-col>
+        </van-row>
+      </div>
+      <div style="margin-top:15rpx;">
+        <van-row style="margin-bottom:15rpx">
+          <van-col span="15" offset="1" class="modal-left">
+            <p>购买数量:</p>
+            <p>剩余:{{product.price}}件</p>
+          </van-col>
+          <van-col span="8">
+            <van-stepper :value="1" @change="onChange"/>
+          </van-col>
+        </van-row>
+        <van-goods-action-button text="确定"/>
+      </div>
+    </van-popup>
+  </div>
 </template>
 <script>
-var Fly=require("flyio/dist/npm/wx") 
-var fly=new Fly
+var Fly = require("flyio/dist/npm/wx");
+var fly = new Fly();
 export default {
-  data () {
+  data() {
     return {
-        id:'',
-        product:[],
-        show:false,
-    }
+      id: "",
+      product: [],
+      show: false,
+      baseUrl:process.env.API_ROOT
+    };
   },
   methods: {
-    getDetail(){
-            const baseUrl=process.env.API_ROOT
-        var url=`${baseUrl}/product/${this.id}`
-        fly.get(url).then(res=>{
-            this.product=res.data
-        })
+    getDetail() {
+      var url = `${this.baseUrl}/product/${this.id}`;
+      fly.get(url).then(res => {
+        this.product = res.data;
+        console.log(res.data)
+      });
     },
     //弹出底部组件
-    onShow(){
-      this.show=true;
+    onShow() {
+      this.show = true;
     },
-    onClose(){
-      this.show=false;
+    onClose() {
+   /*    fly.post(url,{this.product}) */
+      this.show = false;
     }
   },
   mounted() {
-      this.id=this.$root.$mp.query.id
-      this.getDetail()
-  }, 
-}
+    this.id = this.$root.$mp.query.id;
+    this.getDetail();
+  }
+};
 </script>
 <style>
-#goods .value-class{
-  color: red!important;
+#goods .value-class {
+  color: red !important;
 }
-#goods img{
+#goods img {
   width: 100%;
   height: 400rpx;
 }
-.modal-top{
-  padding-bottom:30rpx;
+.modal-top {
+  padding-bottom: 30rpx;
   border-bottom: 1px solid #cccccc;
 }
 .price {
   color: red;
   font-size: 32rpx;
 }
-.modal-left p{
-  font-size:30rpx;
-
+.modal-left p {
+  font-size: 30rpx;
 }
-.modal-left p:nth-of-type(2){
+.modal-left p:nth-of-type(2) {
   color: #cccccc;
 }
 </style>
